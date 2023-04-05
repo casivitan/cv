7fef5bf9c1d700419793ef990ae4a4f4e8dab18c
casivitan <111457450+casivitan@users.noreply.github.com>
2023-04-05 18:10:55 +0300

    Update cacat.php

--- cacat.php
+++ cacat.php
@@ -1,40 +1,19 @@
 <?php
-// blueprint / mold / pattern / factory for objects
 
-class Car
+class Person
 {
-    private $color = 'red';
-    public $weight;
-    private $year;
-    private $availableColors = [
-        'red', 'green', 'blue', 'white'
-    ];
+    public $name = "Jane";
+    private $phone = 123456;
 
-    public function setYear($year)
+    public function __get($propName)
     {
-        $this->year = $year;
+        if ($propName === 'username') (
+            return $this->name;
+        )
+        return "Property \"$propName\" does not exist";
     }
-
-    public function getYear()
-    {
-        return $this->year;
-    }
-
-    public function setColor($color)
-    {
-        if (in_array($color, $this->availableColors)) {
-            $this->color = $color;
-
-        }
-    }
-        public function getColor ()
-            {
-        return $this->color;
-            }
 }
 
-$myCar = new Car();
-$myCar->setColor() = 'white';
-
-echo $myCar->getColor();
-
+// construct
+$p = new Person();
+echo $p->username;
