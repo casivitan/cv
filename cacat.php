37c50a683e536d182c01bbc61ce833b734ecbd33
casivitan <111457450+casivitan@users.noreply.github.com>
2023-04-08 20:38:19 +0300

    Update cacat.php

--- cacat.php
+++ cacat.php
@@ -1,17 +1,59 @@
 <?php
 
-class ParentClass
-{
-    public $property1 = '1';
-    protected $property2 = '2';
-    private $property3 = '3';
+class Person {
+
+    public $name;
+    protected $age;
+    private $phone;
+
+    public function __construct($name, $age, $phone)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->phone = $phone;
+    }
+
+    public function hello(){
+        return "Hello from person";
+    }
 
 }
 
-class ChildClass extends ParentClass
-{
+class Employee extends Person {
+
+    private $salary;
+    public function __construct($name, $age, $phone, $salary)
+    {
+        parent::__construct($name, $age, $phone);
+        $this->salary = $salary;
+    }
+
+    public function hello(){
+        return "Sunt angajatul pe nume: $this->name, si am salariul de $this->salary";
+    }
+}
+
+class Student extends Person {
+
+    private $studentNO;
+    public function __construct($name, $age, $phone, $studentNO)
+    {
+        parent::__construct($name, $age, $phone);
+        $this->studentNO = $studentNO;
+    }
+
+    public function hello()
+    {
+        return "Sunt studentul cu numele $this->name, am $this->age ani si am numarul $this->studentNO!";
+    }
 
 }
 
-$obj = new ChildClass ();
-echo $obj->$property1;
+$employee = new Employee("Casi", 27,123456, 2500);
+$employee2 = new Employee("Andrei", 25,654321, 1000);
+$student = new Student("Ion", 14,432243,2 );
+
+
+echo $employee->hello().PHP_EOL;
+echo $employee2->hello().PHP_EOL;
+echo $student->hello().PHP_EOL;
